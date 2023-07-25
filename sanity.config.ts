7d11d5589ccded18d7bbb594a6806387c6b92f8a
leo <leohthom@gmail.com>
2023-07-25 10:58:29 +0800

    Updated sanity config with project schema

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
+import project from './sanity/schemas/project-schema';
 
 const config = defineConfig({
   projectId: 'bltxlk9k',
@@ -11,6 +12,7 @@ const config = defineConfig({
   ],
   apiVersion: '2023-07-24',
   useCdn: true,
+  schema: { types: [project] },
 });
 
 export default config;
