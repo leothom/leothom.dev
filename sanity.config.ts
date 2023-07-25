dc86a831b9010eda8fad9dc394163dc47009e56c
leo <leohthom@gmail.com>
2023-07-25 11:02:25 +0800

    Added schema barrel file

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
-import project from './sanity/schemas/project-schema';
+import schemas from './sanity/schemas';
 
 const config = defineConfig({
   projectId: 'bltxlk9k',
@@ -12,7 +12,7 @@ const config = defineConfig({
   ],
   apiVersion: '2023-07-24',
   useCdn: true,
-  schema: { types: [project] },
+  schema: { types: schemas },
 });
 
 export default config;
