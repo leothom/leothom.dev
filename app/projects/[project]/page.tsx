f97ee235ea83bce2fd6241586d03306ef53e9025
leo <leohthom@gmail.com>
2023-07-25 16:22:09 +0800

    Added PortableText functionality

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -1,4 +1,5 @@
 import { getProject } from "@/sanity/sanity-utils";
+import { PortableText } from "@portabletext/react";
 
 type Props = {
   params: { project: string }
@@ -11,8 +12,28 @@ export default async function Project({params}: Props) {
   const project = await getProject(slug);
     
   return (
-    <div>
-      {project.name}
+    
+    <div className="max-w-3xl mx-auto py-20">
+      <header className="flex justify-between items-center">
+        <h1 className="bg-gradient-to-r from-orange-400 via-red-600 to-purple-500 bg-clip-text text-transparent text-5xl drop-shadow font-extrabold">{project.name}</h1>
+        <a 
+          href={project.url} 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="bg-gray-100 rounded-lg text-gray-500 font-bold py-3 px-4 whitespace-nowrap hover:bg-gray-200 transition cursor-pointer"
+        >
+            Project
+        </a>
+      </header>
+
+
+      {/* Content goes here */}
+      <div><PortableText value={project.content} /></div>
+
+      {/* Image goes here */}
+
+
     </div>
+
   );
 }
