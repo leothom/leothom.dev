511ec7585261383dd2e1824fc669bb990e6693f4
leo <leohthom@gmail.com>
2023-07-27 13:06:30 +0800

    Mobile navigation changes

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -24,26 +24,28 @@ export default async function RootLayout({
         <header className="flex items-center justify-between">
           <Link 
             href="/"
-            className="bg-gradient-to-r from-orange-400 via-red-600 to-purple-500 dark:from-blue-700 dark:via-gray-500 dark:to-white bg-clip-text text-transparent text-2xl font-extrabold"
+            className="md:block hidden bg-gradient-to-r from-orange-400 via-red-600 to-purple-500 dark:from-blue-700 dark:via-gray-500 dark:to-white bg-clip-text text-transparent text-sm sm:text-2xl font-extrabold"
           >
             LEOTHOM.DEV
           </Link>
-          <div className="flex items-center gap-5 text-sm text-gray-600 dark:text-gray-300">
-            <Link href="https://read.cv/leothom" target='_blank' >My CV</Link>
-            {pages.map((page) => (
-              <Link 
-                key={page._id}
-                href={`/${page.slug}`}
-                className='hover:text-gray-900 hover:underline'
-              >
-                {page.title}
-              </Link>
-            ))}
-            <Link href="mailto:work@leothom.com" target='_blank' >Contact</Link>
-            <DarkModeToggle />
+          <div id="right" className="flex justify-center">
+            <div className="flex items-center gap-5 text-sm text-gray-600 dark:text-gray-300">
+              <Link href="https://read.cv/leothom" target='_blank' >CV</Link>
+              {pages.map((page) => (
+                <Link 
+                  key={page._id}
+                  href={`/${page.slug}`}
+                  className='hover:text-gray-900 hover:underline'
+                >
+                  {page.title}
+                </Link>
+              ))}
+              <Link href="mailto:work@leothom.com" target='_blank' >Contact</Link>
+              <DarkModeToggle />
+            </div>
           </div>
         </header>
-        <main className='py-20'>{children}</main>
+        <main className='py-10 md:py-20'>{children}</main>
       </body>
     </html>
   )
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -7,11 +7,12 @@ export default async function Home() {
 
   return (
     <div className="text-gray-700 dark:text-gray-200">
-      <h1 className='text-7xl font-extrabold bg-gradient-to-r from-orange-400 via-red-600 to-purple-500 dark:from-blue-700 dark:via-gray-500 dark:to-white bg-clip-text text-transparent py-10'>Welcome!</h1>
+      <h1 className='text-4xl sm:text-7xl font-extrabold bg-gradient-to-r from-orange-400 via-red-600 to-purple-500 dark:from-blue-700 dark:via-gray-500 dark:to-white bg-clip-text text-transparent md:py-10 py-5'>Welcome!</h1>
       <p className='mt-3 text-xl'>
         I&apos;m Leo and I&apos;m a front-end web developer, with a track record of creating impactful digital experiences for millions of users.
       </p>
-      <p className='mt-6 text-xl'>As a former online marketer and product manager, I transitioned to programming to bridge the gap between elegant technology and joyful user experiences - delivering solutions that are efficient, intuitive, and user-friendly.</p>
+      <p className='mt-6 text-xl'>As a former online marketer and product manager, I transitioned to programming to get closer to the product and end-user.</p> 
+      <p className='mt-6 text-xl'>I seek to bridge the gap between technology and joyful user experiences - delivering solutions that are efficient, intuitive, and user-friendly.</p>
       <p className='mt-6 text-xl'>This portfolio website is built with Next.js, Tailwind CSS, Typescript and Sanity.io</p>
 
       <h2 className='mt-24 font-bold text-gray-700 dark:text-gray-200 text-3xl'>Projects</h2>
