fba6f422e592a7aec2eb05598ee79c1bd946e547
leo <leohthom@gmail.com>
2023-07-27 11:32:38 +0800

    Implemented "Dark Mode", removed base svgs

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -2,6 +2,7 @@ import { getPages } from '@/sanity/sanity-utils';
 import '../globals.css';
 import type { Metadata } from 'next'
 import Link from 'next/link';
+import DarkModeToggle from './DarkModeToggle';
 
 export const metadata: Metadata = {
   title: 'Leo Thom is a full-stack developer based in Hong Kong',
@@ -17,11 +18,9 @@ export default async function RootLayout({
   // get all pages
   const pages = await getPages();
 
-
-
   return (
     <html lang="en">
-      <body className="max-w-3xl mx-auto py-10 bg-[#F5F5F5]">
+      <body className="max-w-3xl mx-auto p-10">
         <header className="flex items-center justify-between">
           <Link 
             href="/"
@@ -29,7 +28,7 @@ export default async function RootLayout({
           >
             LEOTHOM.DEV
           </Link>
-          <div className="flex items-center gap-5 text-sm text-gray-600">
+          <div className="flex items-center gap-5 text-sm text-gray-600 dark:text-gray-300">
             <Link href="https://read.cv/leothom" target='_blank' >My CV</Link>
             {pages.map((page) => (
               <Link 
@@ -41,6 +40,7 @@ export default async function RootLayout({
               </Link>
             ))}
             <Link href="mailto:work@leothom.com" target='_blank' >Contact</Link>
+            <DarkModeToggle />
           </div>
         </header>
         <main className='py-20'>{children}</main>
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,32 +3,31 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 export default async function Home() {
-
   const projects = await getProjects();
 
   return (
-    <div>
+    <div className="text-gray-900 dark:text-white">
       <h1 className='text-7xl font-extrabold bg-gradient-to-r from-orange-400 via-red-600 to-purple-500 bg-clip-text text-transparent py-10'>Welcome!</h1>
-      <p className='mt-3 text-xl text-gray-600'>
-      I&apos;m Leo and I&apos;m a front-end web developer, with a track record of creating impactful digital experiences for millions of users.
+      <p className='mt-3 text-xl'>
+        I&apos;m Leo and I&apos;m a front-end web developer, with a track record of creating impactful digital experiences for millions of users.
       </p>
-      <p className='mt-6 text-xl text-gray-600'>As a former online marketer and product manager, I transitioned to programming to bridge the gap between elegant technology and joyful user experiences - delivering solutions that are efficient, intuitive, and user-friendly.</p>
-      <p className='mt-6 text-xl text-gray-600'>This portfolio website is built with Next.js, Tailwind CSS, Typescript and Sanity.io</p>
+      <p className='mt-6 text-xl'>As a former online marketer and product manager, I transitioned to programming to bridge the gap between elegant technology and joyful user experiences - delivering solutions that are efficient, intuitive, and user-friendly.</p>
+      <p className='mt-6 text-xl'>This portfolio website is built with Next.js, Tailwind CSS, Typescript and Sanity.io</p>
 
-      <h2 className='mt-24 font-bold text-gray-700 text-3xl'>Projects</h2>
+      <h2 className='mt-24 font-bold text-gray-700 dark:text-gray-200 text-3xl'>Projects</h2>
 
       <div className='mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
       {projects.map((project) => (
         <Link 
           href={`/projects/${project.slug}`}
           key={project._id} 
-          className='border-2 border-gray-500 rounded-lg pd-1 hover:scale-105 hover:border-blue-500 transition'
+          className='border-2 border-gray-500 dark:border-gray-400 rounded-lg pd-1 hover:scale-105 hover:border-blue-500 transition'
         >
           {project.image && (
             <Image
               src={project.image}
               alt={project.name}
-              className='object-cover rounded-lg border border-gray-500'
+              className='object-cover rounded-lg border border-gray-500 dark:border-gray-400'
               width={750}
               height={300}
             />
