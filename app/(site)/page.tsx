b8873a5397f5eee835679364a8969906840b4071
leo <leohthom@gmail.com>
2023-07-27 15:00:55 +0800

    Fix for huge FA icon on load

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,7 @@
 import Link from "next/link";
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCode } from '@fortawesome/free-solid-svg-icons';
 
@@ -13,8 +16,9 @@ export default async function Home() {
       </p>
       <p className='mt-6 text-xl'>As a former online marketer and product manager, I transitioned to programming to get closer to the product and end-user.</p> 
       <p className='mt-6 text-xl'>I seek to bridge the gap between technology and joyful user experiences - delivering solutions that are efficient, intuitive, and user-friendly.</p>
-      <p className='mt-6 text-xl'>This portfolio website is built with Next.js, Tailwind CSS, Typescript and Sanity.io</p>
       <p className='mt-6 text-xl'><FontAwesomeIcon icon={faCode} className="text-gray-600 dark:text-gray-300 mr-2" /><Link href="https://github.com/leothom/leothom.dev" target="_blank" className='hover:text-gray-900 hover:underline underline'>GitHub Repo for this website</Link></p>
+      <p className='mt-6 text-xl'>This portfolio website is built with Next.js, TypeScript, Tailwind CSS and Sanity.io</p>
+     
       
     </div>
   );
