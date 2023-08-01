000f8c7c0acbefef9b2927988eee93cd58150e5f
Leo Thom <work@leothom.com>
2023-08-01 17:20:42 +0800

    Added key to stack button

--- app/(site)/projects/[project]/page.tsx
+++ app/(site)/projects/[project]/page.tsx
@@ -35,8 +35,9 @@ export default async function Project({ params }: Props) {
         />
       </a>
       <div className="mt-2">
-        {project.stack.map((tech) => (
+        {project.stack.map((tech, index) => (
           <button
+            key={index}
             type="button"
             className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg md:text-sm text-xs px-2.5 py-1 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
           >
